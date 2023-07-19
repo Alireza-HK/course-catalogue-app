60d08fad782d10bf2b99285043bd2af325230b09
Alireza <Alireza.Haghighatkhah@gmail.com>
2023-07-19 13:54:56 +0300

    Added a simple logging aspect

---
+++ src/main/java/com/example/catalogue/aspect/LoggingAspect.java
@@ -0,0 +1,38 @@
+package com.example.catalogue.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.example.catalogue.service.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        Object[] args = joinPoint.getArgs();
+        StringJoiner params = new StringJoiner(", ", "(", ")");
+        Arrays.stream(args).map(Object::toString).forEach(params::add);
+
+        logger.info("Before {}.{}() - Parameters: {}", className, methodName, params);
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.catalogue.service.*.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("After {} is called. Result: {}", className + "." + methodName, result);
+    }
+}
